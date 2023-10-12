855d26ac8aa4a93a6c7698465a6ea315c1c0f5e9
Sharon <90740789+schu0902@users.noreply.github.com>
2023-10-12 09:54:18 -0400

    Save food and its explanation into a dict

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import pandas as pd
 
 import evadb
 
-# temporary file paths
 MENU_PATH = os.path.join("evadb_data", "tmp", "menu.csv")
 FOOD_PATH = os.path.join("evadb_data", "tmp", "food.csv")
 
@@ -20,8 +19,6 @@ def cleanup():
         shutil.rmtree("evadb_data")
 
 if __name__ == "__main__":
-    # llm = GPT4All("ggml-model-gpt4all-falcon-q4_0.bin")
-    # llm = GPT4All("orca-mini-3b.ggmlv3.q4_0.bin")
     cursor = evadb.connect().cursor()
     api_key = str(input("🔑 Enter your OpenAI key: "))
     os.environ["OPENAI_KEY"] = api_key
@@ -29,36 +26,28 @@ if __name__ == "__main__":
     if os.path.exists(img_file):
         img = Image.open(img_file)
         ocr_result = pytesseract.image_to_string(img)
-        print(ocr_result)
         content = [{"text": ocr_result}]    
         df = pd.DataFrame(content)
         df.to_csv(MENU_PATH)
     else:
         raise FileNotFoundError
 
-
-    # load chunked transcript into table
     cursor.drop_table("Menu", if_exists=True).execute()
     cursor.query(
         """CREATE TABLE IF NOT EXISTS Menu (text TEXT(50));"""
     ).execute()
     cursor.load(MENU_PATH, "Menu", "csv").execute()
 
-    question = f""" Give me a list of the words that appear on the given text that represent food.
+    question = f""" Give me a list of words that appear on the given text and are food.
     No repeated items. Present in comma separated form.
     """
 
-    q = "What are the colors in a rainbow?"
-
     if len(cursor.table("Menu").select("text").df()["menu.text"]) == 1:
-        print("Capoo")
         response = (cursor.table("Menu")
-            .select(f"ChatGPT('{q}', text)")
-            .df()["chatgpt.response"][0])
-        print(response)
-        print(type(response))
+            .select(f"ChatGPT('{question}', text)")
+            .df()["chatgpt.response"][0]).split(',')
     
-    print(len(response))
+    food = {}
 
     for item in response:
 
@@ -82,11 +71,9 @@ if __name__ == "__main__":
                 .select(f"ChatGPT('{instruction}', text)")
                 .df()["chatgpt.response"][0])
             print(explanation)
-        else:
-            print("XXXX")
+        
+        food[item] = explanation
 
     cleanup()
 
-    print("done")
-
-    
+    print("Session ended")
